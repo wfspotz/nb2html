af13a46787ab345fce077c80743be7f3b145db13
Bill Spotz <wfspotz@sandia.gov>
2018-07-16 14:53:13 -0600

    Add __init__.py file to make nbref a package

---
+++ nbref/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['AddCitationsExporter',
+           'AddCitationsPreprocessor',
+           'VerboseExecutePreprocessor']
+
+from .AddCitationsExporter       import AddCitationsExporter
+from .AddCitationsPreprocessor   import AddCitationsPreprocessor
+from .VerboseExecutePreprocessor import VerboseExecutePreprocessor
